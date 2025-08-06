0372b2b861ea69fe6cdd96d43039bd71f882bf8e
j2232 <noyoyama777@gmail.com>
2025-08-06 16:27:49 +0000

    日付、色

--- routes/index.js
+++ routes/index.js
@@ -3,41 +3,55 @@ const router = express.Router();
 const knex = require('../db/knex');
 
 router.get('/', function (req, res, next) {
-  const isAuth = req.isAuthenticated();
-  if (isAuth) {
-    const userId = req.user.id;
-    knex("tasks")
-      .select("*")
-      .where({user_id: userId})
-      .then(function (results) {
-        res.render('index', {
-          title: 'ToDo App',
-          todos: results,
-          isAuth: isAuth,
+    const isAuth = req.isAuthenticated();
+    if (isAuth) {
+        const userId = req.user.id;
+        const filterColor = req.query.color; // クエリパラメータから色を取得
+
+        let query = knex("tasks").select("*").where({user_id: userId});
+        
+        // クエリパラメータに色情報があり、「all」以外の場合に絞り込み条件を追加
+        if (filterColor && filterColor !== 'all') {
+            query = query.where({color: filterColor});
+        }
+
+        query.then(function (results) {
+            res.render('index', {
+                title: 'ToDo App',
+                todos: results,
+                isAuth: isAuth,
+            });
+        })
+        .catch(function (err) {
+            console.error(err);
+            res.render('index', {
+                title: 'ToDo App',
+                isAuth: isAuth,
+                errorMessage: [err.sqlMessage],
+            });
         });
-      })
-      .catch(function (err) {
-        console.error(err);
+    } else {
         res.render('index', {
-          title: 'ToDo App',
-          isAuth: isAuth,
-          errorMessage: [err.sqlMessage],
+            title: 'ToDo App',
+            isAuth: isAuth,
         });
-      });
-  } else {
-    res.render('index', {
-      title: 'ToDo App',
-      isAuth: isAuth,
-    });
-  }
+    }
 });
 
-router.post('/', function (req, res, next) {
+router.post('/', function(req, res, next) {
+  const todoContent = req.body.add;
+  const deadline = req.body.deadline;
+  const color = req.body.color; // 色情報を受け取る
   const isAuth = req.isAuthenticated();
   const userId = req.user.id;
-  const todo = req.body.add;
+
   knex("tasks")
-    .insert({user_id: userId, content: todo})
+    .insert({
+      user_id: userId,
+      content: todoContent,
+      deadline: deadline,
+      color: color // ここに color を追加
+    })
     .then(function () {
       res.redirect('/')
     })
